e495c570be8c416f8e94263cd503dad484df05d0
Daniel Sehaik <daniel.sehaik@solaredge.com>
2023-06-16 11:25:37 +0300

    fix code

--- worker/app.py
+++ worker/app.py
@@ -6,8 +6,10 @@ import os
 from config import ORCHESTRATOR_IP, TIME_OUT, PORT, EXIT_FLAG
 from datetime import datetime
 
+headers = {"Content-Type": "application/json", 'Accept': 'application/json'}
 
-def work(buffer, iterations):
+
+def do_work(buffer, iterations):
     import hashlib
     output = hashlib.sha512(buffer.encode('utf-8')).digest()
     for i in range(iterations - 1):
@@ -17,23 +19,18 @@ def work(buffer, iterations):
 
 def main():
     start_time = datetime.utcnow()
-    headers = {
-        "Content-Type": "application/json", 'Accept': 'application/json'}
     while True:
         dif = datetime.utcnow() - start_time
         request = requests.get(f'http://{ORCHESTRATOR_IP}:{PORT}/get_work')
         work = request.json()
-
         if work:
-            res = work(work["file"], work["iterations"])
+            res = do_work(work["file"], work["iterations"])
             requests.put(f"http://{ORCHESTRATOR_IP}:{PORT}/get_result", headers=headers,
                          json={"job_id": work["job_id"], "result": str(res)})
             start_time = datetime.utcnow()
-
         else:
             if dif.seconds > TIME_OUT and EXIT_FLAG:
                 os.system('sudo shutdown -h now')
-
         time.sleep(1)
 
 
